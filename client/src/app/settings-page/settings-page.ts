2eedacec550135ce983cb09631d7d33662bee511
James Swearingen <swear051@morris.umn.edu>
2025-02-27 13:50:25 -0600

    create settings page
    created the settings page and added some settings on it
    
    Co-authored-by: Elliot Hanson <hans7680@morris.umn.edu>
    Co-authored-by: Jayden Sieling <sieli028@morris.umn.edu>

---
+++ client/src/app/settings-page/settings-page.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-settings-page',
+  templateUrl: 'settings-page.html',
+  styleUrls: ['./settings-page.scss'],
+  providers: [],
+  imports: [MatCardModule, RouterLink, MatInputModule, MatFormFieldModule, MatSelectModule, FormsModule]
+})
+export class SettingsComponent {
+  judgeOption = signal<string | undefined>(undefined);
+  private judgeOption$ = toObservable(this.judgeOption);
+}
