4660875b4419bbcad40459f7c1ee7e4f7f1a39f3
James Swearingen <swear051@morris.umn.edu>
2025-02-27 14:19:52 -0600

    update settings updated the settings with a checkbox and added a default option for judge selection
    Co-authored-by: Elliot Hanson <hans7680@morris.umn.edu>
    Co-authored-by: Jayden Sieling <sieli028@morris.umn.edu>

--- client/src/app/settings-page/settings-page.ts
+++ client/src/app/settings-page/settings-page.ts
@@ -6,15 +6,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-settings-page',
   templateUrl: 'settings-page.html',
   styleUrls: ['./settings-page.scss'],
   providers: [],
-  imports: [MatCardModule, RouterLink, MatInputModule, MatFormFieldModule, MatSelectModule, FormsModule]
+  imports: [MatCardModule, RouterLink, MatInputModule, MatFormFieldModule, MatSelectModule, FormsModule, MatCheckboxModule]
 })
 export class SettingsComponent {
   judgeOption = signal<string | undefined>(undefined);
   private judgeOption$ = toObservable(this.judgeOption);
+  default = 'cycle';
 }
