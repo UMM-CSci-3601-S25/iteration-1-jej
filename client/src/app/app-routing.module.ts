3df5b8785f3e6ae06f8d1d23326c7c246e5f0a36
James Swearingen <swear051@morris.umn.edu>
2025-02-28 12:53:17 -0600

    delete unneeded things
    deleted company card and user things from previous lab that we aren't using, along with sidebar navigation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './landing-page/landing-page';
-import { AddUserComponent } from './users/add-user.component';
-import { UserListComponent } from './users/user-list.component';
-import { UserProfileComponent } from './users/user-profile.component';
-import { CompanyListComponent } from './company-list/company-list.component';
 import { SettingsComponent } from './settings-page/settings-page';
 import { GameComponent } from './game-page/game-page';
 
@@ -13,10 +9,6 @@ import { GameComponent } from './game-page/game-page';
 // 'users/new'; the router would just think that the string 'new' is a user ID.
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Generic Apples Game'},
-  {path: 'users', component: UserListComponent, title: 'Users'},
-  {path: 'users/new', component: AddUserComponent, title: 'Add User'},
-  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
-  {path: 'companies', component: CompanyListComponent, title: 'Companies'},
   {path: 'settings', component: SettingsComponent, title: 'Settings'},
   {path: 'game', component: GameComponent, title: 'Game'}
 ];
