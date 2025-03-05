b27a8954f4a9a2ad076eef48ec8075001a088712
ElliotHanson25 <hans7680@morris.umn.edu>
2025-03-05 17:35:37 -0600

    Add code to actually access a game in /game
    Co-authored-by: Jayden Sieling <sieli028@morris.umn.edu>

--- client/src/app/game-page/game-page.ts
+++ client/src/app/game-page/game-page.ts
@@ -8,6 +8,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Game } from '../game';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-game-page',
@@ -17,5 +20,19 @@ import { Game } from '../game';
   imports: [MatCardModule, RouterLink, MatInputModule, MatFormFieldModule, MatSelectModule, FormsModule, MatCheckboxModule]
 })
 export class GameComponent {
-  game = input.required<Game>();
+  todo = toSignal(
+    this.route.paramMap.pipe(
+      // Map the paramMap into the id
+      map((paramMap: ParamMap) => paramMap.get('id')),
+      switchMap((id: string) => this.httpClient.get<Game>(`/api/game/${id}`),
+      catchError((_err) => {
+        this.error.set({
+          help: 'There was a problem loading the game – try again.',
+          httpResponse: _err.message,
+          message: _err.error?.title,
+        });
+        return of();
+      })
+
+    )
 }
